5e088dcd592432131c368ef593855d0c7e9c2e9a
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-06-15 17:31:55 -0300

    Update desafio52.py

--- desafio52.py
+++ desafio52.py
@@ -1,8 +1,3 @@
-"""
-EXERCÍCIO 052: Números Primos
-
-Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
-"""
 num = int(input('Digite um número: '))
 tot = 0
 for c in range(1, num + 1):

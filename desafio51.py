3d74d38de8a526fa213f58604035542bc9ca882e
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-06-15 17:31:51 -0300

    Update desafio51.py

--- desafio51.py
+++ desafio51.py
@@ -1,9 +1,3 @@
-"""
-EXERCÍCIO 051: Progressão Aritmética
-
-Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
-No final, mostre os 10 primeiros termos dessa progressão.
-"""
 primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 decimo = primeiro + (10 - 1) * razao

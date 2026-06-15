ed41248015edef2cae2bfcdc064b66b241132eee
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-06-15 17:27:17 -0300

    Update desafio55.py

--- desafio55.py
+++ desafio55.py
@@ -1,9 +1,3 @@
-"""
-EXERCÍCIO 055: Maior e Menor da Sequência
-
-Faça um programa que leia o peso de cinco pessoas.
-No final, mostre qual foi o maior e o menor peso lidos.
-"""
 maior = 0
 menor = 0
 for p in range(1, 6):

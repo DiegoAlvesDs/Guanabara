294c220516e6b35db1fcfdb3bc0280c16046875a
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:06:46 -0300

    Remove mundo-3/desafio86.py (renomeado)

--- mundo-3/desafio86.py
+++
@@ -1,5 +0,0 @@
-# EX086 - Matriz 3x3 (minha versao)
-matriz = [[int(input(f'Valor para [{l}, {c}]: ')) for c in range(3)] for l in range(3)]
-print('-=' * 20)
-for linha in matriz:
-    print(' '.join(f'[{v:^5}]' for v in linha))

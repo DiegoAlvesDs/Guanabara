41eee14cd7c6db6cd2c3fe5a25c20911408d93d3
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:06:40 -0300

    Remove mundo-3/desafio75.py (renomeado)

--- mundo-3/desafio75.py
+++
@@ -1,7 +0,0 @@
-# EX075 - Analise de tupla (minha versao)
-valores = tuple(int(input(f'Numero {i + 1}: ')) for i in range(4))
-print(f'Voce digitou: {valores}')
-print(f'O 9 apareceu {valores.count(9)} vez(es).')
-print(f'O 3 saiu pela primeira vez na {valores.index(3) + 1}a posicao.' if 3 in valores else 'O 3 nao apareceu.')
-pares = [v for v in valores if v % 2 == 0]
-print(f'Valores pares: {pares if pares else "nenhum"}')

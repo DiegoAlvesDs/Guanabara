d46ea0f46404f9bdfc9a87f8b1612a5a295edc42
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:06:44 -0300

    Remove mundo-2/desafio57.py (renomeado)

--- mundo-2/desafio57.py
+++
@@ -1,7 +0,0 @@
-# EX057 - Validacao de sexo (minha versao)
-while True:
-    sexo = input('Informe seu sexo: [M/F] ').strip().upper()
-    if sexo in ('M', 'F'):
-        break
-    print('Valor invalido! Tente de novo.')
-print(f'Sexo {sexo} registrado com sucesso!')

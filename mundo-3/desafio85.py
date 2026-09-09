1e7e39da3eddf323c19b805affa4b0b8dbc116b4
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:05:30 -0300

    Remove mundo-3/desafio85.py (renomeado)

--- mundo-3/desafio85.py
+++
@@ -1,9 +0,0 @@
-# EX085 - Pares e impares separados (minha versao)
-valores = [[], []]  # [pares, impares]
-for i in range(7):
-    n = int(input(f'{i + 1}o. valor: '))
-    valores[n % 2].append(n)
-valores[0].sort()
-valores[1].sort()
-print(f'Pares:   {valores[0]}')
-print(f'Impares: {valores[1]}')

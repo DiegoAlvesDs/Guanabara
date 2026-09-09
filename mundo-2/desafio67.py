5c7130fa19ca8cca7a5e102dd3deeebce48c3603
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:07:07 -0300

    Remove mundo-2/desafio67.py (renomeado)

--- mundo-2/desafio67.py
+++
@@ -1,8 +0,0 @@
-# EX067 - Tabuada v3.0 (minha versao)
-while True:
-    n = int(input('Tabuada de qual valor? (negativo sai) '))
-    if n < 0:
-        print('Ate logo!')
-        break
-    for i in range(1, 11):
-        print(f'{n:>2} x {i:>2} = {n * i:>3}')

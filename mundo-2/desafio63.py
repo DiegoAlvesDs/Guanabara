f2ecd601a72f3dd1584432f0dc32a5f55487ed54
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:05:09 -0300

    Remove mundo-2/desafio63.py (renomeado)

--- mundo-2/desafio63.py
+++
@@ -1,6 +0,0 @@
-# EX063 - Fibonacci (minha versao com lista)
-n = int(input('Quantos termos de Fibonacci? '))
-fib = [0, 1]
-while len(fib) < n:
-    fib.append(fib[-1] + fib[-2])
-print(' -> '.join(str(x) for x in fib[:n]))

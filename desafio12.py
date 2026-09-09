9bce876cc818b3078de30cb119abad358d9eab65
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 13:44:38 -0300

    Organiza: remove desafio12.py da raiz

--- desafio12.py
+++
@@ -1,8 +0,0 @@
-preco = float(input("Digite o preço do produto: R$ "))
-
-desconto = preco * 0.05
-novo_preco = preco - desconto
-
-print(f"\nPreço original : R$ {preco:.2f}")
-print(f"Desconto (5%)  : R$ {desconto:.2f}")
-print(f"Novo preço     : R$ {novo_preco:.2f}")

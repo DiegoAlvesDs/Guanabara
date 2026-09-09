3f4b97d348b6d85fa1fe71312b0a2089c5b9fb1e
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:05:52 -0300

    Remove mundo-2/desafio60.py (renomeado)

--- mundo-2/desafio60.py
+++
@@ -1,7 +0,0 @@
-# EX060 - Fatorial (minha versao com math.prod)
-from math import prod
-
-n = int(input('Numero para o fatorial: '))
-fatores = range(n, 0, -1)
-print(' x '.join(str(f) for f in fatores), end=' = ')
-print(prod(fatores))

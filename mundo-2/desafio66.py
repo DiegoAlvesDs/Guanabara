df271e644edaec10ec2abe634a5bf365578e63e8
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:07:03 -0300

    Remove mundo-2/desafio66.py (renomeado)

--- mundo-2/desafio66.py
+++
@@ -1,8 +0,0 @@
-# EX066 - Varios numeros com flag (minha versao)
-valores = []
-while True:
-    n = int(input('Digite um numero [999 para parar]: '))
-    if n == 999:
-        break
-    valores.append(n)
-print(f'Voce digitou {len(valores)} numero(s) e a soma foi {sum(valores)}.')

e35fe3d4ba937714f382c2745efc3c13d8ac4b0f
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:07:38 -0300

    Remove mundo-2/desafio71.py (renomeado)

--- mundo-2/desafio71.py
+++
@@ -1,8 +0,0 @@
-# EX071 - Caixa eletronico (minha versao com divmod)
-print('=== BANCO DA COBRINHA ===')
-valor = int(input('Valor do saque: R$'))
-for cedula in (50, 20, 10, 1):
-    qtd, valor = divmod(valor, cedula)
-    if qtd:
-        print(f'{qtd} cedula(s) de R${cedula}')
-print('Volte sempre!')

075448867e4829abf05d5d4f0579d9969465b350
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:05:19 -0300

    Remove mundo-1/desafio5.py (renomeado)

--- mundo-1/desafio5.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("Digite um número inteiro: "))
-
-antecessor = numero - 1
-sucessor = numero + 1
-
-print(f"Antecessor: {antecessor}")
-print(f"Número: {numero}")
-print(f"Sucessor: {sucessor}")

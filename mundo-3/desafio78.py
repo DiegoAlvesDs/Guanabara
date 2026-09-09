10c55abf1d6d94b9a23cfc7753db9d199d9ee507
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:05:17 -0300

    Remove mundo-3/desafio78.py (renomeado)

--- mundo-3/desafio78.py
+++
@@ -1,6 +0,0 @@
-# EX078 - Maior e menor na lista (minha versao)
-valores = [int(input(f'Valor na posicao {i}: ')) for i in range(5)]
-maior, menor = max(valores), min(valores)
-print(f'Lista: {valores}')
-print(f'Maior: {maior} nas posicoes {[i for i, v in enumerate(valores) if v == maior]}')
-print(f'Menor: {menor} nas posicoes {[i for i, v in enumerate(valores) if v == menor]}')

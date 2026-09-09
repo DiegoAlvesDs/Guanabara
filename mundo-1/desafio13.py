89ecf8d29d8d238c9e6c4bfe39d0268807d14b6d
DiegoAlvesDs <diegoalveslopes08@gmail.com>
2026-09-09 14:06:56 -0300

    Remove mundo-1/desafio13.py (renomeado)

--- mundo-1/desafio13.py
+++
@@ -1,8 +0,0 @@
-salario = float(input("Digite o salário do funcionário: R$ "))
-
-aumento = salario * 0.15
-novo_salario = salario + aumento
-
-print(f"\nSalário atual : R$ {salario:.2f}")
-print(f"Aumento (15%) : R$ {aumento:.2f}")
-print(f"Novo salário  : R$ {novo_salario:.2f}")
